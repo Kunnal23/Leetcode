ddbc8df4cb4085e5a8074fc0e8dd0dc2c00a5d76
Kunnal <kantkunnal2311@gmail.com>
2025-02-16 21:46:45 +0530

    Problems on String

--- valid_palindrome.py
+++ valid_palindrome.py
@@ -1,3 +1,12 @@
 def isPalindrome(s):
+    # Brute Force
+    # s = "".join(char.lower() for char in s if char.isalnum())
+    # return s == s[::-1]
+
     s = "".join(char.lower() for char in s if char.isalnum())
-    return s == s[::-1]
+    n = len(s)
+    mid = n//2
+    for i in range(mid):
+        if s[i] != s[n-i-1]:
+            return False
+    return True
